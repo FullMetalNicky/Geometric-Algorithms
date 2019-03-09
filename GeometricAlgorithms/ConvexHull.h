a677747e69bbb1d434854a63362df581c62a8d1f
FullMetalNicky <nickyfullmetal@gmail.com>
2019-03-09 23:08:59 +0100

    adding documentation

--- GeometricAlgorithms/ConvexHull.h
+++ GeometricAlgorithms/ConvexHull.h
@@ -13,18 +13,21 @@ public:
 
 	/** @brief computes convex hull using Graham's scan
 		@param P - list of 2D points defining the polygon
+		@param debug - Optionary. An image of the polygon P, for debugging purposes
 		@return a convex polygon given by a list of vertices ordered CCW
 	**/
 	std::vector<cv::Point> GrahamsScan(std::vector<cv::Point> P, cv::Mat debug = cv::Mat());
 
 	/** @brief computes convex hull using Jarvis march
 		@param P - list of 2D points defining the polygon
+		@param debug - Optionary. An image of the polygon P, for debugging purposes
 		@return a convex polygon given by a list of vertices ordered CCW
 	**/
 	std::vector<cv::Point> JarvisMarch(std::vector<cv::Point> P, cv::Mat debug = cv::Mat());
 
 	/** @brief computes convex hull using Chan's algoritm
 		@param P - list of 2D points defining the polygon
+		@param debug - Optionary. An image of the polygon P, for debugging purposes
 		@return a convex polygon given by a list of vertices ordered CCW
 	**/
 	std::vector<cv::Point> ChansAlgorithm(std::vector<cv::Point> P, cv::Mat debug = cv::Mat());
@@ -52,6 +55,7 @@ public:
 	/** @brief finds the right tangent for a query point and a CCW convex hull
 		@param P - list of 2D points defining a CCW convex hull
 		@param q - query point
+		@param debug - Optionary. An image of the polygon P, for debugging purposes
 		@return the index of the vertex in P which is the right tangent
 	**/
 	int FindRightTangent(std::vector<cv::Point> P, cv::Point q, cv::Mat debug = cv::Mat());
@@ -59,6 +63,7 @@ public:
 	/** @brief finds the maximal dot product between a query point and a CCW convex hull
 		@param P - list of 2D points defining a CCW convex hull
 		@param q - query point
+		@param debug - Optionary. An image of the polygon P, for debugging purposes
 		@return the index of the vertex in P which is gives maximal dot product with q
 	**/
 	int FindMaximalDotProduct(std::vector<cv::Point> P, cv::Point q, cv::Mat debug = cv::Mat());
